152ca267316806ce87514a322e166a0fa24ed649
yas-amini <yaasamin.amini@gmail.com>
2025-06-04 13:42:14 +0200

    Created postcss.config to convert tailwind classes to CSS

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // Converts Tailwind utility classes into regular CSS
+    tailwindcss: {},
+    // Adds browser-specific prefixes for compatibility
+    autoprefixer: {},
+  },
+};
