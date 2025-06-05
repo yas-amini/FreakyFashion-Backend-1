0aafa4245323d2d312f7bc8329d7d43f9ffb2990
yas-amini <yaasamin.amini@gmail.com>
2025-06-05 12:17:32 +0200

    Removed Tailwind CSS and related files; updated package.json and package-lock.json to reflect changing too bootsrap.

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // Converts Tailwind utility classes into regular CSS
-    tailwindcss: {},
-    // Adds browser-specific prefixes for compatibility
-    autoprefixer: {},
-  },
-};
